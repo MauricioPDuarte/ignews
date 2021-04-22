2f332f0d045a28a08171edf814fd5c56dcb5c217
Mauricio Duarte <prussdev@gmail.com>
2021-04-21 22:50:38 -0300

    Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
@@ -9,7 +8,7 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>
+      <h1 >
 
         Hello World!
       </h1>
