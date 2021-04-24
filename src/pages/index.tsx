3cab86fa65f1ccba9b2fd92ea3d82ed989132277
Mauricio Pruss Duarte <prussdev@gmail.com>
2021-04-24 15:12:50 -0300

    Pagina Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
@@ -8,10 +9,18 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 >
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
 
-        Hello World!
-      </h1>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
