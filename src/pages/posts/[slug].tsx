b959d3d699160b17f678b365293e0e6d1c86fd3b
Mauricio Pruss Duarte <prussdev@gmail.com>
2021-04-25 19:51:14 -0300

    Validando assinatura ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps } from "next";
 import { getSession } from "next-auth/client";
+import { redirect } from "next/dist/next-server/server/api-utils";
 import Head from "next/head";
 import { RichText } from "prismic-dom";
 import { getPrismicClient } from "../../services/prismic";
@@ -40,6 +41,15 @@ export default function Post({post}: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
     const session = await getSession({ req });
     const { slug } = params;
+
+    if(!session.activeSubscription) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
 
     const prismic = getPrismicClient(req);
 
