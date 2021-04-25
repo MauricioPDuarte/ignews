fe2fb1dc5bfc1cff647488ef3dcac0e2dc7869ba
Mauricio Pruss Duarte <prussdev@gmail.com>
2021-04-24 22:40:02 -0300

    Configurando o FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
+import {} from '../../'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -9,4 +11,12 @@ export default NextAuth({
       scope: 'read:user'   
     }),
   ],
+
+  callbacks: {
+      async signIn(user, account, profile) {
+        console.log(user);
+
+        return true;
+      }
+  }
 })
