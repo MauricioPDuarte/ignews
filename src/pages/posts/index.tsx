cdd5614d4eb1374c892ea0b882f2265d8e2bf752
Mauricio Pruss Duarte <prussdev@gmail.com>
2021-04-25 15:54:48 -0300

    Pagina posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In the guide, you will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process.</p>                  
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In the guide, you will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process.</p>                  
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In the guide, you will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process.</p>                  
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+                        <p>In the guide, you will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process.</p>                  
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
